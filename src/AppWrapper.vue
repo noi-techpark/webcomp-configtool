0ada9a22fdca17e9267b5055f36c1c6730350b85
Andreas Hufler <andreas.hufler@konverto.eu>
2019-10-22 09:27:40 +0200

    some refactoring to allow for direct integration in other vue projects via node_modules;

---
+++ src/AppWrapper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <WcsConfigurator
+      :config="getOptions"
+      v-if="show"
+      v-on:snippet="emitResult"
+    ></WcsConfigurator>
+    <div v-if="error">
+      Error while loading config.
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import WcsConfigurator from './components/wcs-configurator';
+import BootstrapVue from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
+export default {
+  components: { WcsConfigurator },
+  data() {
+    return {
+      show: false,
+      error: false,
+      config: {
+        options: []
+      },
+      snippet: null
+    };
+  },
+  computed: {
+    getOptions() {
+      return this.config.options;
+    }
+  },
+  mounted() {
+    setTimeout(this.initConfig, 100);
+  },
+  methods: {
+    initConfig() {
+      if (this.$slots.default && this.$slots.default[0]) {
+        try {
+          this.config = JSON.parse(this.$slots.default[0].text);
+          this.show = true;
+        } catch (e) {
+          this.error = true;
+        }
+      } else {
+        this.error = true;
+      }
+    },
+    emitResult(snippet) {
+      this.$emit('snippet', snippet);
+    }
+  }
+};
+</script>
