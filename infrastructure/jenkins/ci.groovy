251b93a68512c332e0e135d7ad7c6c0e25c9f42a
Peter Moser <p.moser@noi.bz.it>
2021-02-09 21:49:53 +0100

    infra: new infrastructure folder

--- infrastructure/jenkins/ci.groovy
+++ infrastructure/jenkins/ci.groovy
@@ -1,29 +1,27 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'infrastructure/docker/Dockerfile'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+            filename 'infrastructure/docker/node.dockerfile'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
         }
     }
-
     options {
         ansiColor('xterm')
     }
-
     stages {
         stage('Dependencies') {
             steps {
-                sh 'yarn install'
+                sh 'yarn'
             }
         }
         stage('Test') {
             steps {
-                sh 'yarn run lint'
+                sh 'yarn lint'
             }
         }
         stage('Build') {
             steps {
-                sh 'yarn run build'
+                sh 'yarn build'
             }
         }
     }
